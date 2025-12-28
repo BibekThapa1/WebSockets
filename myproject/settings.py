048c802fbcd56a94eacd976c594083b9110c4abf
bibekthapa1 <bibekthapa00f@gmail.com>
2025-12-28 22:55:30 +0545

    feat: add routing.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'myapp'
 ]
+ASGI_APPLICATION = "myproject.asgi.application"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
