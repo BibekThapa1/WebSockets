10042945ec4e3e9e5758cda1a964ab466e906036
bibekthapa1 <bibekthapa00f@gmail.com>
2025-12-29 20:26:14 +0545

    feat: update asgi application file

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,16 +1,18 @@
 import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from myapp.routing import websocket_urlpatterns
+import myapp.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+print("🔥 ASGI APPLICATION LOADED")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            myapp.routing.websocket_urlpatterns
         )
     ),
 })
